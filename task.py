2c4b4e8fa3a0baaaa09a86a823413272e9e130c3
juliasenzon3 <142810926+juliasenzon3@users.noreply.github.com>
2024-03-06 21:28:41 +0000

    completed code

--- task.py
+++ task.py
@@ -1,10 +1,11 @@
-drivingSpeed = 69
+drivingSpeed = 65
 
 if drivingSpeed <= 55:
-    print ("You are driving within the speed limit.")
+    print("You are driving within the speed limit.")
+#If person drives under or equal to 55 mph, say they are driving within the speed limit.
 
 if drivingSpeed >= 56 and drivingSpeed <= 65:
-    print ("You are driving slightly above the speed limit.")
+    print("You are driving slightly above the speed limit.")
 
 if drivingSpeed >= 66:
-    print ("You are driving considerably above the speed limit.")
+    print("You are driving considerably above the speed limit.")
