958f6e280545e96ff0a143124e1bd832df4d3605
juliasenzon3 <142810926+juliasenzon3@users.noreply.github.com>
2024-03-06 19:32:06 +0000

    completed code

--- task.py
+++ task.py
@@ -1,2 +1,10 @@
-hello = 0
-print("This line will be printed.")
+drivingSpeed = 69
+
+if drivingSpeed <= 55:
+    print ("You are driving within the speed limit.")
+
+if drivingSpeed >= 56 and drivingSpeed <= 65:
+    print ("You are driving slightly above the speed limit.")
+
+if drivingSpeed >= 66:
+    print ("You are driving considerably above the speed limit.")
